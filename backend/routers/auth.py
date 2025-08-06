dc335bdc7f10bf1c7360dda3b35565d929bd9e7b
KEVIN OSSMAR PEDROZA HERNANDEZ <kevin-oph@users.noreply.github.com>
2025-08-06 15:02:59 -0600

    Update auth.py

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from schemas import Token, UserCreate, User
-from backend.routers.users import create_access_token, authenticate_user, get_current_user
+from routers.users import create_access_token, authenticate_user, get_current_user
 from crud import create_user
 
 router = APIRouter(prefix="/api/auth", tags=["auth"])
